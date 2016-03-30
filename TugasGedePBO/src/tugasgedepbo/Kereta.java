75825e1fbebc88543a3c1d0030ad0ff34f7d2340
iwannabd <azaqokay@gmail.com>
2016-03-30 21:08:01 +0700

    aplikasi belum jadi

--- TugasGedePBO/src/tugasgedepbo/Kereta.java
+++ TugasGedePBO/src/tugasgedepbo/Kereta.java
@@ -5,11 +5,65 @@
  */
 package tugasgedepbo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author iwannadb
  */
 public class Kereta {
-    String nama;
-    int JumlahGerbong;
+    private ArrayList<Gerbong> gerbongs;
+    private String nama;
+    private int maxGerbong;
+    private Rute rute;
+    
+    public Kereta(String nama,int maxGer){
+        this.nama = nama;
+        maxGerbong = maxGer;
+    }
+    
+    public void addGerbong(Gerbong g){
+        if (gerbongs.size()<getMaxGerbong()) 
+            gerbongs.add(g);
+        else
+            System.out.print("penuh");
+    }
+    
+    private ArrayList<Gerbong> getGerbongs(){
+        return gerbongs;
+    }
+    public void setRoute(Rute ru){
+        this.rute = ru;
+    }
+    public Rute getRoute(){
+        return rute;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the maxGerbong
+     */
+    public int getMaxGerbong() {
+        return maxGerbong;
+    }
+
+    /**
+     * @param maxGerbong the maxGerbong to set
+     */
+    public void setMaxGerbong(int maxGerbong) {
+        this.maxGerbong = maxGerbong;
+    }
 }
